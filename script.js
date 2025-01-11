b2c7c30113055043d96fb94f999cbabbe01007f6
fragranceland <fragranceland.perfumes@gmail.com>
2025-01-11 15:36:18 +0400

    number change

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+
+
+var glide = new Glide("#intro1", {
+    type: "carousel",
+    perView: 4,
+    gap: 100,
+    autoplay: 3000,
+    // hoverpause: false, 
+    animationDuration: 3000, // Set the transition duration to 1000ms (1 second)
+    animationTimingFunc: 'linear',
+    rewind: false,      
+    pagination: {
+        el: ".glide__bullet",
+    },
+    arrows: {
+        prev: ".slider-prev",
+        next: ".slider-next",
+    },
+    breakpoints: {
+        1200: { perView: 3,  }, // Show 4 slides on screens larger than 1200px
+        900: { perView: 2 },  // Show 2 slides on screens between 900px and 1200px
+        600: { perView: 2, gap: 10, },  // Show 1 slide on smaller screens
+    }
+});
+glide.mount();
+
+
 var glide = new Glide("#intro", {
     type: "carousel",
-    perView: 6,
+    perView: 7,
     gap: 100,
     autoplay: 3000,
     hoverpause: false, 
@@ -21,33 +48,6 @@ var glide = new Glide("#intro", {
     }
 });
 glide.mount();
-
-var glide = new Glide("#intro1", {
-    type: "carousel",
-    perView: 5,
-    gap: 100,
-    autoplay: 3000,
-    hoverpause: false, 
-    animationDuration: 3000, // Set the transition duration to 1000ms (1 second)
-    animationTimingFunc: 'linear',
-    rewind: false,      
-    pagination: {
-        el: ".glide__bullet",
-    },
-    arrows: {
-        prev: ".slider-prev",
-        next: ".slider-next",
-    },
-    breakpoints: {
-        1200: { perView: 3,  }, // Show 4 slides on screens larger than 1200px
-        900: { perView: 2 },  // Show 2 slides on screens between 900px and 1200px
-        600: { perView: 2, gap: 10, },  // Show 1 slide on smaller screens
-    }
-});
-glide.mount();
-
-
-
 
 
 /*=============== SHOW MENU ===============*/
